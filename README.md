9e111a162cb011ed598efe1277fb29f7aac943d7
ElsaYilinWang <elsa.yilin.w39@gmail.com>
2024-09-15 13:05:49 +0100

    MongoDB connection established

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ ref: https://www.youtube.com/watch?v=wrHTcjSZQ1Y
 ## Backend
 - Setup Node, Express App w/ TypeScript
 - Integrate MongoDB connection
-- 
+- Register Login on OpenAI portal, get API keys
+- Build a Secure Authentication with Tokens, and HTTP-only signed cookies
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
-import express from "express";
-
-const app = express();
-
-// middleware
-app.use(express.json());
+import app from "./app.js";
+import { connectToDatabase } from "./db/connections";
 
 // connections and listeners
+const PORT=process.env.PORT || 5000;
+connectToDatabase()
+  .then(()=>{
 
-app.listen(5000, () => console.log("Server Open"))
+  app.listen(PORT, () => console.log("Server Open & Connected To Database"))
+
+  })
+  .catch((err) => console.log(err));
+
