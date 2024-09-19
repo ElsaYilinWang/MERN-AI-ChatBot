beb4181d8a34b2f9edca3fdb071b36b27a055a91
elsawang <elsa.yilin.w39@gmail.com>
2024-09-19 13:44:50 +0100

    import CORS in the backend and allow frontend server to make requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,15 +6,21 @@ import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 config();
 
 const app = express();
 
 // middleware
+
+// CORS
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
+
 app.use(express.json());
 // cookie
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
 
 
 // remove in production
