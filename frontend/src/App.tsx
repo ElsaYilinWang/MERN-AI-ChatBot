05da8d10c4b7d75030b303de587fce1eb4e8d814
elsawang <elsa.yilin.w39@gmail.com>
2024-09-22 21:44:17 +0100

    Signup page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,25 @@ import Login from "./pages/Login";
 import Signup from "./pages/SIgnup";
 import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
+import { useAuth } from "./context/AuthContext";
 
+const auth = useAuth();
 
 function App() {
-
-  return <main>
-    <Header />
-    <Routes>
-      <Route path="/" element={ <Home />} />
-      <Route path="/login" element={ <Login />} />
-      <Route path="/signup" element={ <Signup />} />
-      <Route path="/chat" element={ <Chat />} />
-      <Route path="*" element={ <NotFound />} />
-    </Routes>
-  </main>;
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -21,7 +21,7 @@ export const checkAuthStatus = async () => {
 };
 
 export const sendChatRequest = async (message: string) => {
-  const res = await axios.post("/chat/new", { message});
+  const res = await axios.post("/chat/new", { message });
   if (res.status !== 200) {
     throw new Error("Unable to send chat");
   }
@@ -54,6 +54,20 @@ export const logoutUser = async () => {
   const res = await axios.get("/user/logout");
   if (res.status !== 200) {
     throw new Error("Unable to logout user.");
+  }
+
+  const data = await res.data;
+  return data;
+};
+
+export const signupUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const res = await axios.post("/user/signup", { name, email, password });
+  if (res.status !== 201) {
+    throw new Error("Unable to signup");
   }
 
   const data = await res.data;
